680dc72a32eb66268cc7df627fc2daca88cdcece
Reza Ashena <Ashena.reza@gmail.com>
2021-01-04 13:09:26 -0500

    Create New Solution and API project add new Controller Product

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "List of the products";
+        }
+
+        //[HttpGet("{id}")]
+        //public string GetProduct(int id)
+        //{
+        //    return "Single product";
+        //}
+
+        [HttpGet()]
+        public string GetProduct()
+        {
+            return "Single product";
+        }
+    }
+}
