60d8f22db4faa36fc18c452551f88fb645079e8d
Reza Ashena <Ashena.reza@gmail.com>
2021-01-04 18:43:52 -0500

    Add Sqlit database by EF and add  run SqlCeVsToolbox.4.7.670.vsix to vs have access to read Sqlite database and change product controller to get data from Sqlite Database Asyncronize

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +13,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
         {
-            return "List of the products";
+            _context = context;
         }
 
-        //[HttpGet("{id}")]
-        //public string GetProduct(int id)
-        //{
-        //    return "Single product";
-        //}
-
-        [HttpGet()]
-        public string GetProduct()
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "Single product";
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }
