bb0ec0f45852ed7b7ee588b3f43ea202dffb7734
Reza Ashena <Ashena.reza@gmail.com>
2021-01-05 14:37:50 -0500

    Configure Migration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -12,7 +12,7 @@ namespace Core.Entities
 
         public string Description { get; set; }
 
-        [Column(TypeName = "decimal(18,2)")]
+        //[Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
         public string PictureUrl { get; set; }
