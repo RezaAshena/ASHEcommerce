9b94e4e280a16b3ff3dee381f4d4cd6df480a20c
Reza Ashena <Ashena.reza@gmail.com>
2021-01-06 07:23:03 -0500

    Implement consistent error handeling for API (Section 5)

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad Request, you have made.",
+                401 => "Autorized, you are not",
+                404 => "Resource not found",
+                500 => "Server Error",
+                _ => null
+            };
+        }
+
+
+
+    }
+}
