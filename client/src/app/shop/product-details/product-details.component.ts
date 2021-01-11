dd73febb1e6ce669943c8db17e0d0c26d30f519d
Reza Ashena <Ashena.reza@gmail.com>
2021-01-11 11:36:19 -0500

    Hooking up the product detail component to the basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from '../../basket/basket.service';
 import { IProduct } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 
@@ -12,13 +13,30 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
 
   product: IProduct;
+  quantity = 1;
 
   constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
-                                                private bcService: BreadcrumbService) { }
+                                                private bcService: BreadcrumbService,
+                                                private BasketService:BasketService) { }
 
   ngOnInit(): void {
     this.loadProduct();
   }
+
+  addItemToBasket() {
+    this.BasketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  } 
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+        this.quantity--;
+    }
+ 
+  } 
 
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
