7cf6c987ca0f92987373b9f88ec09aaccdbedbeb
Reza Ashena <Ashena.reza@gmail.com>
2021-01-13 10:55:24 -0500

    Section 17 creating the accoun module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
   { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: { breadcrumb: 'Checkout' } },
-  {path: '**',redirectTo: 'not-found',pathMatch:'full'}
+  { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } } },
+  { path: '**', redirectTo: 'not-found', pathMatch:'full'}
 ];
 
 @NgModule({
