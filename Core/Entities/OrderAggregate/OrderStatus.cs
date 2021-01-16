e28250068a7849fb7038c8d96a54a2803e9872e8
Reza Ashena <Ashena.reza@gmail.com>
2021-01-16 11:38:14 -0500

    Creating the order aggregate section 18

---
+++ Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Core.Entities.OrderAggregate
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value= "Pending")]
+        Pending,
+
+        [EnumMember(Value = "Payment Recived")]
+        PaymentRecived,
+
+        [EnumMember(Value = "Payment Failed")]
+        PaymentFailed,
+    }
+}
