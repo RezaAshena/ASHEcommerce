da35d2af1469981c09155fd1f4b9f673d5044f66
Reza Ashena <Ashena.reza@gmail.com>
2021-01-16 12:59:21 -0500

    Creating an order service

---
+++ Infrastructure/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
