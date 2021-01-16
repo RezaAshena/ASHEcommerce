f97f37a4f95b75983c92e89eb028faeb77870527
Reza Ashena <Ashena.reza@gmail.com>
2021-01-16 14:04:55 -0500

    Creating the order controller

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -21,6 +21,8 @@ namespace API.Helpers
             CreateMap<Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
+
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>(); //this is a diferent address
         }
     }
 }
