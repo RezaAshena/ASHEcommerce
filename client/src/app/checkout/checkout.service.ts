83068095906806fa7645c1b6cb0c471ac9538bc0
Reza Ashena <Ashena.reza@gmail.com>
2021-01-18 17:02:00 -0500

    Creating the order interface, order submission

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { IDeliveryMethod } from '../shared/models/deliveryMethod';
+import { IOrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class CheckoutService {
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+
+  createOrder(order: IOrderToCreate) {
+    return this.http.post(this.baseUrl + 'orders', order);
+  }
 
   getDeliveryMethods() {
     return this.http.get(this.baseUrl + 'orders/deliveryMethods').pipe(
