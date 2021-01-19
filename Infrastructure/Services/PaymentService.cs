9487966cf1b2726a06bd388e0be2be4e576aef47
Reza Ashena <Ashena.reza@gmail.com>
2021-01-18 20:15:58 -0500

    Settingup stripe(create account) and install stripe package for infrastructure project add keys tp appsetting and then creating the payment service and interface

---
+++ Infrastructure/Services/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
