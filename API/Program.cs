81131e2d386f7828254c04f24d5a3a60584e637f
Reza Ashena <Ashena.reza@gmail.com>
2021-01-26 19:50:27 -0500

    Change appsettings.json to show images in Content folder: "ApiUrl": "https://ashenaweb.com/Content/" Also change Programs file to have access to server by: webBuilder.UseStartup<Startup>()                              .UseUrls("http://localhost:5000");

--- API/Program.cs
+++ API/Program.cs
@@ -48,7 +48,8 @@ namespace API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                             .UseUrls("http://localhost:5000");
                 });
     }
 }
